cdca765e2161d985012ad0ec53781efc34103926
Damian Lewis <damian@damianlewis.co.uk>
2019-12-04 10:02:02 +0000

    Removed custom validation message

--- models/Settings.php
+++ models/Settings.php
@@ -11,47 +11,15 @@ class Settings extends Model
 
     use Validation;
 
-    /**
-     * Implemented behaviours.
-     *
-     * @var array
-     */
     public $implement = [
         SettingsModel::class,
     ];
 
-    /**
-     * Unique key.
-     *
-     * @var array
-     */
     public $settingsCode = 'instagram_settings';
 
-    /**
-     * Field definitions.
-     *
-     * @var array
-     */
     public $settingsFields = 'fields.yaml';
 
-
-    /**
-     * The rules to be applied to the data.
-     *
-     * @var array
-     */
     public $rules = [
-        'access_token' => [
-            'required',
-        ],
-    ];
-
-    /**
-     * The messages used when validation fails.
-     *
-     * @var array
-     */
-    public $customMessages = [
-        'access_token.required' => 'An access token is required.',
+        'access_token' => 'required'
     ];
 }
